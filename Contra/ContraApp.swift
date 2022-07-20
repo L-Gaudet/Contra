4546c8a821d73d21c81a50f1e2686b577a2efb51
Gauds <lucg18@gmail.com>
2022-07-20 16:59:35 -0700

    Initial Commit

---
+++ Contra/ContraApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContraApp.swift
+//  Contra
+//
+//  Created by Lucas Gaudet on 2022-07-20.
+//
+
+import SwiftUI
+
+@main
+struct ContraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
